7924b253540282b608d08703b6f5baf330bf128e
yuragoit <101989870+yuragoit@users.noreply.github.com>
2023-08-23 14:27:54 +0300

    Update vercel_build.sh

--- vercel_build.sh
+++ vercel_build.sh
@@ -5,5 +5,6 @@ python3 -m pip install backports.lzma
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput --clear
-gunicorn --config gunicorn-cfg.py core.wsgi
+# gunicorn --config gunicorn-cfg.py core.wsgi
+python3 manage.py runserver 0.0.0.0:8000
 echo "BUILD END"
