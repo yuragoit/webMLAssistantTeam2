1d685012a6299463535de020852e5061685774ca
yuragoit <101989870+yuragoit@users.noreply.github.com>
2023-08-23 11:39:35 +0300

    Update vercel_build.sh

--- vercel_build.sh
+++ vercel_build.sh
@@ -1,9 +1,9 @@
 echo "BUILD START"
-python3.10 -m pip install --upgrade pip
-python3.10 -m pip install --no-cache-dir -r requirements.txt
-python3.10 -m pip3 install db-sqlite3
-python3.10 manage.py makemigrations
-python3.10 manage.py migrate
-python3.10 manage.py collectstatic --noinput --clear
+python3 -m pip install --upgrade pip
+python3 -m pip install --no-cache-dir -r requirements.txt
+python3 -m pip3 install db-sqlite3
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py collectstatic --noinput --clear
 #gunicorn --config gunicorn-cfg.py core.wsgi
 echo "BUILD END"
