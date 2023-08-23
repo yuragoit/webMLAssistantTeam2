5c0b6c5365fb52b412da5efad40f197a452f8dd3
yuragoit <101989870+yuragoit@users.noreply.github.com>
2023-08-23 22:40:47 +0300

    Update vercel_build.sh

--- vercel_build.sh
+++ vercel_build.sh
@@ -1,7 +1,6 @@
 echo "BUILD START"
 python3 -m pip install --upgrade pip
 python3 -m pip install --no-cache-dir -r requirements.txt
-python3 -m pip install backports.lzma
 python3 manage.py makemigrations
 python3 manage.py migrate
 # python3 manage.py collectstatic --noinput --clear
