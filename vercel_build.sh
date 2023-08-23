99b38029c2d809f26acbdbd56b91a897c4c4f90d
yuragoit <101989870+yuragoit@users.noreply.github.com>
2023-08-23 11:30:58 +0300

    Update vercel_build.sh

--- vercel_build.sh
+++ vercel_build.sh
@@ -1,8 +1,7 @@
 echo "BUILD START"
-apt-get install libsqlite3-dev
 python3.9 -m pip install --upgrade pip
 python3.9 -m pip install --no-cache-dir -r requirements.txt
-python3.9 -m pip install pysqlite
+python3.9 -m pip3 install db-sqlite3
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
 # python3.9 manage.py collectstatic --noinput --clear
