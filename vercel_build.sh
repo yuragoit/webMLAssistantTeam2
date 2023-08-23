03440fadf21860104cf1f6094929e16aeaa96106
yuragoit <101989870+yuragoit@users.noreply.github.com>
2023-08-23 11:06:16 +0300

    Update vercel_build.sh

--- vercel_build.sh
+++ vercel_build.sh
@@ -1,8 +1,8 @@
- echo "BUILD START"
- pip install --upgrade pip
- python -m pip install --no-cache-dir -r requirements.txt
- python manage.py makemigrations
- python manage.py migrate
- python manage.py collectstatic --noinput --clear
+echo "BUILD START"
+python -mpip install --upgrade pip
+python -m pip install --no-cache-dir -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic --noinput --clear
 #gunicorn --config gunicorn-cfg.py core.wsgi
- echo "BUILD END"
+echo "BUILD END"
